447e4de18782386072b32b5b6a69e4ce7bd2c555
Jonathan Cremin <jonathan@crem.in>
2018-10-31 11:06:35 +0000

    Switch social link to Mastodon

--- src/components/Blurb.js
+++ src/components/Blurb.js
@@ -5,7 +5,7 @@ export default function Blurb() {
     <div id="about">
       I'm an engineering manager living in Dublin and working at <a href="https://www.udemy.com">Udemy</a>.
     I've built stuff for Mac, iOS, and the web, like <a href="https://hostr.co">Hostr</a> and <a href="https://combine.fm">Combine.fm</a>.
-    Want to get in touch? Email <a href="mailto:jonathan@crem.in">jonathan@crem.in</a> or tweet <a href="https://twitter.com/kudoz">@kudoz</a>.
+    Want to get in touch? Email <a href="mailto:jonathan@crem.in">jonathan@crem.in</a> or toot <a rel="me" href="https://mastodon.social/@kudos">@kudos</a>.
     </div>
   );
 }
